1ba76cadeec2978170e3d26ef24971bc7dd2b863
Henk <henkrandd@gmail.com>
2024-04-20 09:19:04 +0200

    fix author service

--- src/main/java/guru/springframework/spring6webapp/controllers/AuthorController.java
+++ src/main/java/guru/springframework/spring6webapp/controllers/AuthorController.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6webapp.controllers;
 
+import guru.springframework.spring6webapp.services.AuthorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,14 +9,16 @@ import guru.springframework.spring6webapp.repositories.AuthorRepository;
 
 @Controller
 public class AuthorController {
-    private final AuthorRepository authorRepository;
-    AuthorController(AuthorRepository authorRepository) {
-        this.authorRepository = authorRepository;
-    }    
+
+    final private AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
 
     @RequestMapping("/authors")
     public String getAuthors(Model model) {
-        model.addAttribute("authors", authorRepository.findAll());
+        model.addAttribute("authors", authorService.findAll());
         return "authors";
     }
 }
